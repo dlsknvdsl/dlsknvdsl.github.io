2fc906d356ab94bfda841309f55ad626c3a9d421
dlsknvdsl <142402526+dlsknvdsl@users.noreply.github.com>
2024-01-31 23:17:49 +0800

    Added Atom's, Shek's, Rolan's Scheds

--- scheds.js
+++ scheds.js
@@ -40,5 +40,39 @@ const scheds =
         "GEd109": [null, null, null, null, "15:00-16:30", "7:00-8:30"],
         "CpE401": [null, null, null, null, "16:30-18:00", "14:30-16:00"],
         "NSTP121CW": [null, null, null, null, null, null, "7:00-10:00"]
+    },
+    "Atom": {
+        "SS041": [null, "9:30-10:45", null, "9:30-10:45", null, "9:30-10:45"],
+        "SS032": [null, "10:45-12:00", null, "10:45-12:00", null, "10:45-12:00"],
+        "ENT078": [null, "12:00-14:30", "12:00-13:15"],
+        "PSY101": [null, null, "8:15-10:45", null, "8:15-9:30"],
+        "MATH035": [null, null, "10:45-12:00", null, "10:45-12:00", null, "10:45-12:00"],
+        "PE042": [null, null, null, "7:00-9:30"],
+        "NSTP021": [null, null, null, null, null, null, "8:15-10:45"]
+    },
+    "Rolan": {
+        "GEC4000": [null, "13:00-14:00", null, "13:00-14:00", null, "13:00-14:00"],
+        "NAT100-3": [null, "14:00-15:00", null, "14:00-15:00", null, "14:00-15:00"],
+        "AMT1210": [null, "15:00-18:00"],
+        "AMT1212L": [null, "18:00-21:00"],
+        "AMT1208": [null, null, "11:30-13:00", null, null, "8:30-10:00"],
+        "AMT1209": [null, null, "13:00-15:00"],
+        "AMT1211L": [null, null, null, "7:30-9:00,18:00-19:30"],
+        "FCL1101": [null, null, null, "10:00-12:00"],
+        "AMT1210L": [null, null, null, "15:00-18:00", "15:00-18:00"],
+        "AMT1211": [null, null, null, null, "13:00-14:00"],
+        "AMT1208L": [null, null, null, null, "18:00-21:00", "18:00-12:00"],
+        "PE200": [null, null, null, null, null, "10:00-12:00"],
+        "AMT1209L": [null, null, null, null, null, "15:00-18:00"],
+        "NTP200": [null, null, null, null, null, null, "9:00-12:00"]
+    },
+    "Shek": {
+        "PHYS72": [null, null, "10:15-11:15", "10:00-11:30", null, "10:00-11:30"],
+        "MATH22": [null, null, "11:45-12:45", "11:45-12:45", "11:45-12:45", "11:45-12:45"],
+        "EEE123": [null, null, "13:00-14:30", null, "13:00-14:30"],
+        "EEE121": [null, null, "16:15-19:15", null, "8:30-11:30,16:15-17:15"],
+        "FIL40": [null, null, null, "13:00-14:30", null, "13:00-14:30"],
+        "EEE128": [null, null, null, "16:00-19:00"],
+        "PE2": [null, null, null, null, null, "7:00-9:00"]
     }
 };
